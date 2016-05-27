07e65e65ba0d89cf5742b7fb2ca926fee3a4b893
Savva Mikhalevski <smikhalevski@gmail.com>
2016-05-27 21:38:50 +0300

    Testing colgroup support

--- page/index.js
+++ page/index.js
@@ -9,6 +9,37 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
+        <table style={{width: '100%'}}>
+          <colgroup>
+            <col style={{width: '50px'}}/>
+            <col style={{width: 'calc(100% - 100px)'}}/>
+            <col style={{width: '50px'}}/>
+            <col style={{width: '100px'}}/>
+          </colgroup>
+          <thead>
+          <tr>
+            <th colSpan="2">A</th>
+            <th>B</th>
+            <th rowSpan="2">C</th>
+          </tr>
+          <tr>
+            <th>A1</th>
+            <th>A2</th>
+            <th>B1</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>a1</td>
+            <td>a2</td>
+            <td>b1</td>
+            <td>c</td>
+          </tr>
+          </tbody>
+        </table>
+
+
+
         <Table structure={{colgroups: [{id: 'default'}], headers: [
           {
             caption: 'AAA',
